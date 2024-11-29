3b09440c5b12afe80f9447b152d6321cc0db9917
XolaniMba <xolanimbambo22@gmail.com>
2024-11-29 10:25:48 +0200

    Home.jsx styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,54 +1,55 @@
-import React from "react"
-import Slider from "react-slick"
-import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
-import HomeCard from "./HomeCard"
+import React from "react";
+import Slider from "react-slick"; // Import Slider from react-slick
+import "slick-carousel/slick/slick.css"; // Slick carousel CSS
+import "slick-carousel/slick/slick-theme.css"; // Slick carousel theme CSS
+import HomeCard from "./HomeCard"; // Import HomeCard for displaying individual items
 
+// Next Arrow for the carousel
 const SampleNextArrow = (props) => {
-  const { onClick } = props
+  const { onClick } = props;
   return (
-    <div className='control-btn' onClick={onClick}>
-      <button className='next'>
-        <i class='fa fa-chevron-right'></i>
+    <div className="control-btn" onClick={onClick}>
+      <button className="next">
+        <i className="fa fa-chevron-right"></i>
       </button>
     </div>
-  )
-}
-const SamplePrevArrow = (props) => {
-  const { onClick } = props
-  return (
-    <div className='control-btn' onClick={onClick}>
-      <button className='prev'>
-        <i class='fa fa-chevron-left'></i>
-      </button>
-    </div>
-  )
-}
-const Home = ({ items }) => {
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    nextArrow: <SampleNextArrow />,
-    prevArrow: <SamplePrevArrow />,
-  }
-  return (
-    <>
-      <div className='homeContainer'>
-        <Slider {...settings}>
-          {items.map((item) => {
-            return (
-              <>
-                <HomeCard key={item.id} item={item} />
-              </>
-            )
-          })}
-        </Slider>
-      </div>
-    </>
-  )
-}
+  );
+};
 
-export default Home
+// Previous Arrow for the carousel
+const SamplePrevArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <div className="control-btn" onClick={onClick}>
+      <button className="prev">
+        <i className="fa fa-chevron-left"></i>
+      </button>
+    </div>
+  );
+};
+
+const Home = ({ items }) => {
+
+  const settings = {
+    dots: true, 
+    infinite: true, 
+    speed: 500, 
+    slidesToShow: 1, 
+    slidesToScroll: 1, 
+    nextArrow: <SampleNextArrow />, 
+    prevArrow: <SamplePrevArrow />, 
+  };
+
+  return (
+    <div className="homeContainer">
+      {/* Render Slider */}
+      <Slider {...settings}>
+        {items.map((item) => (
+          <HomeCard key={item.id} item={item} /> 
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default Home;
